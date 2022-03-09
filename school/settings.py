a465f36665984700da3dd7bef62757a8c865fd56
Rovine1999 <rovinewanjala99@gmail.com>
2022-03-09 23:55:47 +0300

    Deployment to heroku

--- school/settings.py
+++ school/settings.py
@@ -20,7 +20,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
